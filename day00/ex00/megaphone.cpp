2ec0ad4d65f94ab8860c41f542b381b6bc7dca26
Mpho Mathivha <mpho@Mpho.local>
2019-06-04 18:29:08 +0200

    all

---
+++ day00/ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(int argc, char **argv) {
+ int i = 1;
+ int j = 0;
+ if (argc <= 1)
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  while(i < argc) {
+      j = 0;
+      while(argv[i][j] != '\0') {
+         argv[i][j] = toupper(argv[i][j]);
+        j++;
+      }
+        std::cout << argv[i] << std::endl;
+        i++;
+    } 
+  
+    return 0;
+
+}
