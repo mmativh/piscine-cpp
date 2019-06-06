7bf4728dbef377b22b4775dd8c9d2f8e3fa0be22
Mpho MATHIVHA <mmathivh@c4r6s8.wethinkcode.co.za>
2019-06-06 13:09:58 +0200

    all

--- day01/ex05/Human.cpp
+++ day01/ex05/Human.cpp
@@ -1,22 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Human.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmathivh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 13:07:35 by mmathivh          #+#    #+#             */
+/*   Updated: 2019/06/06 13:07:39 by mmathivh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Human.hpp"
 #include "Brain.hpp"
 #include <string>
 #include <iostream>
 
-Human::Human() : brain("drake", 200)
-{
+Human::Human() : brain("drake", 200){
 }
 
-Human::~Human()
-{
+Human::~Human() {
 }
 
-std::string Human::identify(void) const
-{
+std::string Human::identify(void) const {
 	return this->brain.identify();
 }
 
-const Brain& Human::getBrain(void) const
-{
+const Brain& Human::getBrain(void) const {
 	return brain;
 }
